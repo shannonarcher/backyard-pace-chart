7cf5d99bd8692731f2863c8cabffa8444df810ab
Shannon Archer <shannon.archer0@gmail.com>
2025-07-25 15:15:31 -0500

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Backyard Ultra Pace Chart",
+        short_name: "PaceBackyard",
+        description: "A tool to calculate and chart running paces.",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
